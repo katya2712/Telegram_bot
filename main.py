671c82d330309e2b573dc6e2304aadbd89dbd6c2
Andrey Syutkin <syutkin@gmail.com>
2024-02-07 21:06:07 +0300

    Пробное изменение

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from input_parser import input_parser
 
 if __name__ == '__main__':
-    print('Я бот')
+    print('Я телеграм бот')
     while True:
         input_parser(input('Введите команду: '))
