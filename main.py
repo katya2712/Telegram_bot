f450bb5c1268805dcb458626aa9e3cbe345427aa
Andrey Syutkin <syutkin@gmail.com>
2024-02-08 21:19:19 +0300

    Получение ключа TMDb из файла

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from tmdbv3api import TMDb
 from input_parser import input_parser
 
 if __name__ == '__main__':
+    with open('tmdb.key') as file:
+        tmdb_key = file.readline()
     tmdb = TMDb()
-    tmdb.api_key = ''
+    tmdb.api_key = tmdb_key
     tmdb.language = 'ru'
     print('Я телеграм бот')
     while True:
